88a924399eaba9e9ee0f0c807aba13675be1d1c3
Gabriel Raimundo <gabriel.alesiunas@yahoo.com.br>
2023-04-03 14:30:16 -0300

    Projeto Finalizado

---
+++ Componentes/x.js
@@ -0,0 +1,34 @@
+import {StyleSheet} from "react-native";
+
+const x = StyleSheet.create({
+    container: {
+      flex: 1,
+
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: 'black',
+    },
+    card: {
+      width: 350,
+      height: 250,
+      backgroundColor: 'grey',
+      marginBottom: 30,
+    },
+    card_tiutlo: {
+      textAlign: 'center',
+      padding: 5,
+      fontSize: 24,
+      fontWeight: 'bold',
+      backgroundColor: '#00000061',
+      color: 'white',
+    },
+    card_valor: {
+      textAlign: 'center',
+      fontSize: 20,
+      backgroundColor: '#00000061',
+      color: 'white',
+      marginTop: 192,
+    },
+  });
+
+  export default x;
